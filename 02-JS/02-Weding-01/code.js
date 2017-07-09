2a59465a5e8bc3b0d2175ae42f9faeb1bf595cf0
Edxael <zerphyr.r@gmail.com>
2017-07-09 00:09:19 -0600

    transfor to RSVP'

--- 02-JS/02-Weding-01/code.js
+++ 02-JS/02-Weding-01/code.js
@@ -1,15 +1,34 @@
-console.log("Hello from Code.js");
 
 var data = [];
 
+
+function rsvp(name, number, email){
+  this.name = name;
+  this.reservations = number;
+  this.email = email;
+}
+
+
 function addName(){
   var name = document.getElementById("input-1").value;
-  console.log(name);
+  var number = document.getElementById("input-2").value;
+  var email = document.getElementById("input-3").value;
 
-  var newP = document.createElement("p");
-  newP.innerHTML = name;
-  newP.className = "nameClass";
+  document.getElementById("input-1").value = "";
+  document.getElementById("input-2").value = "";
+  document.getElementById("input-3").value = "";
 
-  nameHolder.insertBefore(newP, nameHolder.childNodes[0]);
+  if(name !== ""){
+    var rsvpToPush = new rsvp(name, number, email);
+    data.push(rsvpToPush);
+  }
+
+  document.getElementById("thanks").innerHTML = name + " you have " + number + " reservations, thanks for RSVP" ;
+
+  setTimeout(() => {
+    document.getElementById("thanks").innerHTML = "Please provide info and then Click RSVP";
+  }, 3000);
+
+
 
 }
