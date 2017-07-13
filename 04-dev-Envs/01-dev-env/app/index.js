e4811f7c2f6ce7b94cf1b1debb097aa796c77921
Edxael <zerphyr.r@gmail.com>
2017-07-13 17:09:44 -0600

    updtate on files

--- 04-dev-Envs/01-dev-env/app/index.js
+++ 04-dev-Envs/01-dev-env/app/index.js
@@ -1,11 +1,10 @@
 console.log("Hello from index.js in folder: app");
 
 
-// var root = 'https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699';
+var root = 'https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699';
 
-// fetch(root, { method: "GET"})
-fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch((err) => console.log(err));
-// ------------------------------------
+fetch(root, { method: 'GET'})
+  .then((response) => response.json())
+  // .then((data45) => console.log(data45.items[0].volumeInfo.title) );
+  .then((data45) => document.getElementById("div1").innerHTML = data45.items[0].volumeInfo.title )
+  .catch((err) => document.getElementById("div1").innerHTML = "Book Data Not available");
