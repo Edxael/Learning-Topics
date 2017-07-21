2b33f5dd367a72f96d9f4a7c8ac15f898b907359
Edxael <zerphyr.r@gmail.com>
2017-07-21 08:26:42 -0600

    13 wed camp

---
+++ 02-JS/02-constUTJS-Practice/13-wed/code.js
@@ -0,0 +1,37 @@
+console.log("code.js");
+
+const reservations_db = [];
+
+
+class RecordConstructor {
+  constructor(data1, data2, data3) {
+    this.name = data1;
+    this.invites = data2;
+    this.email = data3;
+  }
+}
+
+
+const addRecord = () => {
+
+  const data1 = document.getElementById("in-1").value;
+  const data2 = document.getElementById("in-2").value;
+  const data3 = document.getElementById("in-3").value;
+
+  document.getElementById("in-1").value = "";
+  document.getElementById("in-2").value = "";
+  document.getElementById("in-3").value = "";
+
+  if(data1 !== ""){
+
+    const record = new RecordConstructor(data1, data2, data3);
+    reservations_db.push(record);
+    console.log(reservations_db);
+
+    document.getElementById("p1").innerHTML = `${data1} your ${data2} will be send to your email.`;
+
+    setTimeout(() => {
+      document.getElementById("p1").innerHTML = "Please provide information above then click RSVP";
+    },4000);
+  }
+}
