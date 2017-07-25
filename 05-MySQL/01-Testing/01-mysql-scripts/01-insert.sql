f27526b5d6b5bcaf9e95bd6f3da2f5f1213685ce
Edxael <zerphyr.r@gmail.com>
2017-07-25 08:50:23 -0600

    MySQL 4 Projects Folder

---
+++ 05-MySQL/01-Testing/01-mysql-scripts/01-insert.sql
@@ -0,0 +1,6 @@
+CREATE TABLE web_dev
+  (
+    fname VARCHAR(25),
+    lname VARCHAR(25),
+    age INT
+  );
---
+++ 05-MySQL/01-Testing/01-mysql-scripts/02-insert.sql
@@ -0,0 +1,12 @@
+-- To run this script:
+-- source 01-mysql-scripts/02-insert.sql
+
+
+INSERT INTO web_dev(fname, lname, age)
+    VALUES
+        ('Ayumi', 'Hamasaki', 22),
+        ('Laura', 'Pausini', 18),
+        ('Eros', 'Ramazzoti', 20);
+
+
+-- SELECT * FROM web_dev;
