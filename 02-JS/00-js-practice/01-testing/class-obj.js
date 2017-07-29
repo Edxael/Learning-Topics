43cab89afa897a83de349441d1286cb3f4deb05a
Edxael <zerphyr.r@gmail.com>
2017-07-29 08:25:22 -0600

    Shoot the zombies

---
+++ 02-JS/00-js-practice/01-testing/class-obj.js
@@ -0,0 +1,28 @@
+console.log("Hello from Code.js");
+
+// Wat is an array: a box where we can storage things.
+// but you need to know the exact INDEX
+//
+// drawer storage
+// clothes storage
+
+// Here is where we come to a beautiful structure.
+
+var singer = {
+  name: "Ayumi Hamasaki",
+  age: 38,
+  dob: "10/02/78"
+  country: "Japan"
+  generes: ["J-Pop", "Dance", "Electronic", "Rock", "Clasical"],
+  website: "www.avex.jp/ayu",
+  favsongs: ["Memorial Address", "Who", "Beloved", "Step You", "Heaven"]
+}
+
+
+create
+access [dot & bracket]
+change
+Address
+delete
+
+looping 
