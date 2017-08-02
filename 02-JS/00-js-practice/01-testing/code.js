7f8b80b9721aa1333f8bb643b91395adc4936b2b
Edxael <zerphyr.r@gmail.com>
2017-08-02 09:59:54 -0600

    add items

--- 02-JS/00-js-practice/01-testing/code.js
+++ 02-JS/00-js-practice/01-testing/code.js
@@ -1,1 +1,8 @@
-console.log("Hello from Code.js");
+console.log("Hello from Code 5.js");
+
+
+var myArray1 = ["apple", "peach", "banana", "mango", "avocado"];
+console.log(myArray1);                // ["apple", "peach", "banana", "mango", "avocado"]
+
+myArray1.invert();
+console.log(myArray1);
